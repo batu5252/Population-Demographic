b154fab162fa665ac15c9f0c541be2fc857f819d
Batu Özdemir <76134828+batu5252@users.noreply.github.com>
2024-03-02 01:33:51 +0300

    Population-Demographic

---
+++ SQLQuery.sql
@@ -0,0 +1,7 @@
+echo "# Population-Demographic" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:batu5252/Population-Demographic.git
+git push -u origin main
